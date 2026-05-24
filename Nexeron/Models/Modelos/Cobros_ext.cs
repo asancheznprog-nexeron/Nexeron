fbe8ac8eec326db297b548ab0f13c41ef4f37f3a
asancheznprog-nexeron <asancheznprog@gmail.com>
2026-05-24 15:16:14 +0200

    cobros

---
+++ Nexeron/Models/Modelos/Cobros_ext.cs
@@ -0,0 +1,8 @@
+﻿namespace Nexeron.Models
+{
+    public partial class cobros
+    {
+        public string NombreCliente { get; set; }
+        public string NombreFormaCobro { get; set; }
+    }
+}
